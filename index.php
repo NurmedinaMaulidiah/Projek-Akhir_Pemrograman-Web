e2d8c0d523da783b09e95b9c5e44a4c079ec7781
NurmedinaMaulidiah <nurmedinamaulidiah@gmail.com>
2022-10-31 10:07:51 +0800

    pertama commit

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link  href='https://fonts.googleapis.com/css2?family=Bungee+Spice&family=Dancing+Script&family=Dangrek&family=Roboto:ital,wght@1,300&display=swap'>
+    <link  href='https://fonts.googleapis.com/css2?family=Bungee+Spice&family=Dancing+Script&family=Dangrek&family=Roboto:ital,wght@1,300&display=swap'>
+    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kdinzy</title>
+    <link rel="stylesheet" href="style4.css">
+    <link rel="stylesheet" href="#">
+    <script src="jquery.js"></script>
+    <style media="screen">
+      *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+body{
+    background-color :#f5b0b3;
+}
+.container{
+    width: 2430px;
+    position: absolute;
+    transform: translate(-50%,-50%);
+    top: 50%;
+    left: 10%;
+    overflow: hidden;
+    border: 0px solid #ffffff;
+    border-radius: 5px;
+    box-shadow: 0px 0px 0px rgba(0,0,0,0.3);
+}
+.wrap{
+    width: 100%;
+    display: flex;
+    animation: slide 16s infinite;
+}
+@keyframes slide{
+    0%{
+        transform: translateX(0);
+    }
+    25%{
+        transform: translateX(0);
+    }
+    30%{
+        transform: translateX(-100%);
+    }
+    50%{
+        transform: translateX(-100%);
+    }
+    55%{
+        transform: translateX(-200%);
+    }
+    75%{
+        transform: translateX(-200%);
+    }
+    80%{
+        transform: translateX(-300%);
+    }
+    100%{
+        transform: translateX(-300%);
+    }
+}
+img{
+    height : 450px
+}
+    </style>
+</head>
+<body style = background-color : #ca4153;>
+<nav>
+        <div class="wrapper">
+            <div class="menu">
+                <ul>
+                    <li class="button1"><a href="#"><b>Home</b></a></li>
+                    <li class="button1"><a href="halaman baru.html"><b>About Us</b></a></li>
+                    <li class="button1"><a href="input-user.php"><b>Sign In</b></a></li>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+    
+
+    
+
+    <div class="container">
+        <div class="wrap">
+            <img src="https://pbs.twimg.com/media/FgNK1LMVIAEN6v0?format=png&name=900x900">
+            <img src="https://pbs.twimg.com/media/FgNK1LMVIAEN6v0?format=png&name=900x900">
+            <img src="https://pbs.twimg.com/media/FgPOet0VUAAx3kS?format=png&name=900x900">
+            <img src="https://pbs.twimg.com/media/FgPOet0VUAAx3kS?format=png&name=900x900">
+            <img src="https://pbs.twimg.com/media/FgPOet0VUAAx3kS?format=png&name=900x900">
+            <img src="https://pbs.twimg.com/media/FgPOet0VUAAx3kS?format=png&name=900x900">
+            <img src="https://pbs.twimg.com/media/FgNK1LMVIAEN6v0?format=png&name=900x900">
+            <img src="https://pbs.twimg.com/media/FgNK1LMVIAEN6v0?format=png&name=900x900">
+            <img src="https://pbs.twimg.com/media/FgPOet0VUAAx3kS?format=png&name=900x900">
+            <img src="https://pbs.twimg.com/media/FgPOet0VUAAx3kS?format=png&name=900x900">
+            <img src="https://pbs.twimg.com/media/FgPOet0VUAAx3kS?format=png&name=900x900">
+            <img src="https://pbs.twimg.com/media/FgPOet0VUAAx3kS?format=png&name=900x900">
+            <img src="https://pbs.twimg.com/media/FgPOet0VUAAx3kS?format=png&name=900x900">
+            <img src="https://pbs.twimg.com/media/FgPOet0VUAAx3kS?format=png&name=900x900">
+            <img src="https://pbs.twimg.com/media/FgPOet0VUAAx3kS?format=png&name=900x900">
+            <img src="https://pbs.twimg.com/media/FgPOet0VUAAx3kS?format=png&name=900x900">
+            <img src="https://pbs.twimg.com/media/FgNK1LMVIAEN6v0?format=png&name=900x900">
+            <img src="https://pbs.twimg.com/media/FgNK1LMVIAEN6v0?format=png&name=900x900">
+            
+        </div>
+    </div>
+
+<div class = "konten">
+    <div class="heading">
+    <h1>Our Specialty</h1>
+    </div>
+    <main class = "grid" >
+        <article>
+            <img src="resto.jpg" width="300px" height="300px">
+            <div class="paragraf">
+            <h2>High Technology</h2>
+            <p>fasilitas self-service pada setiap mejanya dengan menggunakan iPad, sehingga kamu dapat dengan mudah memesan menu, request lagu, berfoto-foto, hingga memanggil pelayan kami hanya dengan sekali sentuhan.</p>
+            </div>
+        </article>
+        <article>
+            <img src="fresh.jpg" width="300px" height="300px">
+            <div class="paragraf">
+            <h2>Fresh Ingridients</h2>
+            <p>Semua resep yang digunakan merupakan resep asli dari K'dinzy, dimasak dengan menggunakan bahan-bahan berkualitas, diproses dengan teknik khusus, dan dikontrol dengan standard quality assurance yang ketat.</p>
+            </div>
+        </article>
+        <article>
+            <img src="live.jpg" width="300px" height="300px">
+            <div class="paragraf">
+            <h2>K-Entertainment</h2>
+            <p>Ingin menghadirkan suasana ke-Korea-an, kami menyediakan musik KPOP dan K-Drama yang selalu up-to-date yang akan menemanimu selama kamu makan.</p>
+            </div>
+        </article>
+    </main>
+    <div class="heading1">
+    <h1>-Visit Us-</h1>
+    </div>
+    <main class = "grid2" >
+        <article>
+            <div class="paragraf">
+            <h2>Bandung</h2>
+            <p>Cihampelas Walk, Young Street</p>
+            </div>
+        </article>
+        <article>
+            <div class="paragraf">
+            <h2>Samarinda</h2>
+            <p>Big Mall, Lt.02</p>
+            </div>
+        </article>
+        <article>
+            <div class="paragraf">
+            <h2>Bekasi</h2>
+            <p>Summarecon Mall Bekasi, Lobby Oval, GF</p>
+            </div>
+        </article>
+</div>
+
+
+
+    
+</body>
+</html>
+    
+    
+    
+    
+    
+    
